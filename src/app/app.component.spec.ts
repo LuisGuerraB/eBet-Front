4445c41de8d4055618b7d417d3b80acca5ae8c8c
LuisGuerraB <luisguebat@hotmail.com>
2023-05-15 00:19:32 +0200

    Migration to standalone Api

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,20 +4,13 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+    imports: [RouterTestingModule, AppComponent]
+}));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'eBet-Front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('eBet-Front');
   });
 
   it('should render title', () => {
