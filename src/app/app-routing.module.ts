49ab005225b4d1996b57fe60854d23ca6b77d865
LuisGuerraB <luisguebat@hotmail.com>
2023-05-18 21:31:23 +0200

    EB-6 User SignUp page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {BetComponent} from "./bet/bet.component";
+import {UserComponent} from "./user/user.component";
+import {RegisterComponent} from "./user/register/register.component";
 
 const routes: Routes = [
-  {path:'', redirectTo:'/bet', pathMatch:'full'},
-  {path:'bet', component:BetComponent}
+  {path: '', redirectTo: '/bet', pathMatch: 'full'},
+  {path: 'bet', component: BetComponent},
+  {path: 'user', component: UserComponent,
+    children: [
+      {path: 'register', component: RegisterComponent},
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
